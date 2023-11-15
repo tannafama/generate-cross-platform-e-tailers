7ecf80a268f57a708cfcf60410b7268f3d208771
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-15 10:12:47 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,38 @@
-public class RandomDataGenerator {
-    public static void main(String[] args) {
+def main():
+    return data
+    for item in data:
+
+if __name__ == "__main__":
+def generate_random_data():
+
+import random
+
+    main()
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]    public static void main(String[] args) {
             System.out.println("Random Number: " + item);
-        }
         int[] data = new int[10];
-        for (int item : data) {
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
         }
 
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
 
 }
-        for (int i = 0; i < 10; i++) {
+    }
+def generate_random_data():
+import random
+    return data
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+    for item in data:
+
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
