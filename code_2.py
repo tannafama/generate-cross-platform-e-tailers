fa0b254b87b0689d2d532097e1d45b4acb63a3a7
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-15 10:56:55 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,52 +1,14 @@
+    main()
+    for item in data:
+def generate_random_data():
+def main():
+
+
+import random
+
+        print(f"Random Number: {item}")
+
     data = generate_random_data()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-    return data
-def main():
-if __name__ == "__main__":
-    main()
-    for item in data:
-        print(f"Random Number: {item}")
-def generate_random_data():
-
     data = [random.randint(1, 100) for _ in range(10)]
     return data
-
-import random
-
-
-def generate_random_data():
-    main()
-    for item in data:
-        print(f"Random Number: {item}")
-def main():
 if __name__ == "__main__":
-    data = generate_random_data()
-
-def main():
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-    main()
-import random
-    for item in data:
-
-
-if __name__ == "__main__":
-    return data
-
-    data = generate_random_data()        int[] data = new int[10];
-
-            data[i] = (int) (Math.random() * 100) + 1;
-}
-        }
-        }
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-    }
-public class RandomDataGenerator {
-        for (int item : data) {
-
-    public static void main(String[] args) {
