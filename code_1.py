e9fff88a8195f8d25998b59925cd184006112416
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-15 11:18:58 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,39 +1,55 @@
-
-    data = generate_random_data()
 def main():
+
     main()
-def generate_random_data():
+    data = generate_random_data()
+if __name__ == "__main__":
+
+import random
     for item in data:
 
     data = [random.randint(1, 100) for _ in range(10)]
-
-if __name__ == "__main__":
-
     return data
-        print(f"Random Number: {item}")
-import random
-}
-        }
 
-    }
-public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-        for (int item : data) {
-        }
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;    for item in data:
-
-
-        print(f"Random Number: {item}")
-    main()
-import random
 def generate_random_data():
-    return data
+        print(f"Random Number: {item}")    return data
 
 
-    data = generate_random_data()
+        print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    data = generate_random_data()
+
 if __name__ == "__main__":
 def main():
+
+def generate_random_data():
+    for item in data:
+import random            data[i] = (int) (Math.random() * 100) + 1;
+
+    }
+        for (int item : data) {
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+
+        for (int i = 0; i < 10; i++) {
+}
+        int[] data = new int[10];
+        }
+    public static void main(String[] args) {
+        }
+}
+    return data;
+function main() {
+
+    const data = generateRandomData();
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+
+        console.log("Random Number: " + item);
+main();
+}
+function generateRandomData() {
+    }
+    const data = [];
+    });
