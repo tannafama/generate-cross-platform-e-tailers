5f04b4d90fa99e682dae3415953e3f730bc7c597
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-15 09:50:41 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,27 +1,52 @@
-
-
+    data = generate_random_data()
 
     data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+    return data
+def main():
+if __name__ == "__main__":
+    main()
+    for item in data:
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+import random
+
+
+def generate_random_data():
+    main()
+    for item in data:
+        print(f"Random Number: {item}")
+def main():
 if __name__ == "__main__":
     data = generate_random_data()
-    for item in data:
-    main()
 
 def main():
-def generate_random_data():
         print(f"Random Number: {item}")
-import random
-    return data    data = [random.randint(1, 100) for _ in range(10)]
-    main()
+    data = [random.randint(1, 100) for _ in range(10)]
 def generate_random_data():
+    main()
+import random
+    for item in data:
+
+
 if __name__ == "__main__":
     return data
-    data = generate_random_data()
 
+    data = generate_random_data()        int[] data = new int[10];
 
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        }
+        }
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+    }
+public class RandomDataGenerator {
+        for (int item : data) {
 
-    for item in data:
-
-        print(f"Random Number: {item}")
-import random
-def main():
+    public static void main(String[] args) {
