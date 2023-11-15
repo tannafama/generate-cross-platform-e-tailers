9e7eef5098d4612439a34aa9d619eb1fdadcba8e
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-11-15 08:44:23 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,27 @@
+
+    for item in data:
+    main()
 def generate_random_data():
 
-    main()
-import random
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-
-        print(f"Random Number: {item}")
+    return data
 def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
 
 if __name__ == "__main__":
-
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+import randomif __name__ == "__main__":
+import random
     return data
+
+
+
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    data = generate_random_data()
+def generate_random_data():
+    main()
+    for item in data:
